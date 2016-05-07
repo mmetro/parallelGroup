7541d1c765b58a98f2b2b3cf8be2f1947569f5f1
Nevin Jacob <kn2ply@gmail.com>
2016-05-06 22:19:27 -0400

    forgot common heap; will add comments later btw

--- run.bat
+++ run.bat
@@ -2,5 +2,6 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jacobn2@rpi.edu
 #SBATCH -t 60
+export  BG_MAPCOMMONHEAP=1
 srun antfarm $1 $2 $3 $4
 # additional calls to srun may be placed in the script, they will all use the same partition 
--- runall.bat
+++ runall.bat
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export  BG_MAPCOMMONHEAP=1
 sbatch --partition debug               --nodes 4   -n 4     -o output00t-004n2000m500a100000f.txt  --job-name 0b32n2000m500a100000f  ./run.bat 0  2000 500000 3000000
 sbatch --partition debug               --nodes 4   -n 4     -o output16t-004n2000m500a100000f.txt  --job-name 8b32n2000m500a100000f  ./run.bat 16 2000 500000 3000000
 sbatch --partition debug               --nodes 8   -n 8     -o output00t-008n2000m500a100000f.txt  --job-name 0b32n2000m500a100000f  ./run.bat 0  2000 500000 3000000
