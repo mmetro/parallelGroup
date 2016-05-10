1c697024875732d2dcc965170e2c88fc78339a8d
Nevin Jacob <kn2ply@gmail.com>
2016-05-09 20:32:16 -0400

    batch files

---
+++ run.bat
@@ -0,0 +1,7 @@
+#!/bin/sh
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jacobn2@rpi.edu
+#SBATCH -t 60
+export  BG_MAPCOMMONHEAP=1
+srun antfarm $1 $2 $3 $4 $5
+# additional calls to srun may be placed in the script, they will all use the same partition 
